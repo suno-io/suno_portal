361b9033aa24deffc9edbc5d88f5ba564fa0d720
George Awwad <george@oddcastles.com>
2017-02-11 03:59:55 -0500

    init

---
+++ app/routes.js
@@ -0,0 +1,54 @@
+import { getAsyncInjectors } from './utils/asyncInjectors';
+
+const errorLoading = (err) => {
+  console.error('Dynamic page loading failed', err); // eslint-disable-line no-console
+};
+
+const loadModule = (cb) => (componentModule) => {
+  cb(null, componentModule.default);
+};
+
+export default function createRoutes(store) {
+  const { injectReducer, injectSagas } = getAsyncInjectors(store);
+
+  return [
+    {
+      path: '/',
+      name: 'home',
+      getComponent(nextState, cb) {
+        const importModules = Promise.all([
+          import('containers/HomePage/reducer'),
+          import('containers/HomePage/sagas'),
+          import('containers/HomePage'),
+        ]);
+
+        const renderRoute = loadModule(cb);
+
+        importModules.then(([reducer, sagas, component]) => {
+          injectReducer('home', reducer.default);
+          injectSagas(sagas.default);
+
+          renderRoute(component);
+        });
+
+        importModules.catch(errorLoading);
+      },
+    }, {
+      path: '/features',
+      name: 'features',
+      getComponent(nextState, cb) {
+        import('containers/FeaturePage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    }, {
+      path: '*',
+      name: 'notfound',
+      getComponent(nextState, cb) {
+        import('containers/NotFoundPage')
+          .then(loadModule(cb))
+          .catch(errorLoading);
+      },
+    },
+  ];
+}
